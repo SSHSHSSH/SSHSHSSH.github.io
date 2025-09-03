7efea1c604c16797c635c3412b32f2ac82403b28
SSHSHSSH <he2367991090@gmail.com>
2025-09-04 00:31:54 +0800

    Site updated: 2025-09-04 00:31:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/29/Github_Pages/","2023/08/06/hello-world/","2025/08/18/HJX_C/"];function toRandomPost(){
+var posts=["2025/08/18/HJX_C/","2025/08/29/Github_Pages/","2023/08/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

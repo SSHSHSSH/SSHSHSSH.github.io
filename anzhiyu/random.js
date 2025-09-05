d5db47031e0f45aefab35e864819bb50decbae27
SSHSHSSH <he2367991090@gmail.com>
2025-09-05 16:53:55 +0800

    Site updated: 2025-09-05 16:53:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/18/HJX_C/","2025/08/29/Github_Pages/","2023/08/06/hello-world/"];function toRandomPost(){
+var posts=["2025/08/29/Github_Pages/","2025/08/18/HJX_C/","2023/08/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
